bbaf4543d38650f015665155af1d5f9d5bd77d54
consultoradiagonales <info.consultoradiagonales@gmail.com>
2026-05-31 18:34:07 -0300

    Actualiza key publica y funciones WhatsApp

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,6 +1,6 @@
 window.CD_SUPABASE = {
   url: "https://fmtjbfufuprkfwneokuk.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtdGpiZnVmdXBya2Z3bmVva3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4Njk2NzUsImV4cCI6MjA5NTQ0NTY3NX0.dd-pwy8qIbTBCmIySbqafE06Q_pQob_QfZsG5lHg3g4",
+  anonKey: "sb_publishable_uyzkF055kqQYzmTY1mJaRQ_53M35zXL",
 };
 
 window.CD_ADMIN = {
